b35a6e21ec8adf51914f60952e8546fc5c3dc6e8
raj u <r@Rs-MacBook-Air.local>
2020-03-24 14:22:56 -0700

    mvp

---
+++ auth/router.js
@@ -0,0 +1,66 @@
+const bcrypt = require("bcryptjs");
+const router = require("express").Router();
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  const userInfo = req.body;
+  // the pasword will be hashed and re-hashed 2 ^ 8 time
+  const ROUNDS = process.env.HASHING_ROUNDS || 8;
+  const hash = bcrypt.hashSync(userInfo.password, ROUNDS);
+  userInfo.password = hash;
+  Users.add(userInfo)
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => res.send(err));
+});
+
+
+router.post("/login", (req, res) => {
+
+    const { username, password } = req.body;
+
+    Users.findBy({ username })
+    
+    .then(([user]) => {
+        if (user && bcrypt.compareSync(password, user.password)){
+            req.session.user = {
+                id: user.id,
+                username: user.username
+            };
+
+        
+
+
+            res.status(200).json({ whats_good: user.username })
+             
+
+        } else {
+            res.status(401).json({ message: 'invalid credentials'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ errorMessage: 'error finding the user'})
+    })
+
+  });
+
+  
+
+  router.get('/logout', (req, res) => {
+      if(req.session) {
+          req.session.destroy(error => {
+              if(error) {
+                  res.status(500)
+                  .json({ message: 'you can checkout any time you like, but you can never leave'})
+
+              } else {
+                  res.status(200).json({ message: 'logged out successful'})
+              }
+          });
+
+      } else {
+          res.status(200).json({ message: "I don't know you"})
+      }
+    })
+module.exports = router;
